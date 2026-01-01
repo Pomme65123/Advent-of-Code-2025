2ba47380d3efd42128389057e21a9050d37857c4
RovasFlow <shan65123@gmail.com>
2026-01-01 17:58:07 -0500

    Solved Day 7 Part 1

--- Day 7/mainPartOne.java
+++ Day 7/mainPartOne.java
@@ -1,20 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
+
+
 
 class mainPartOne {
 
-    private static List<String> lines;
-    private static char[][] grid;
-
-
-
-    // Need to track the beams
     static class Beam {
         int row, col;
 
@@ -22,60 +14,87 @@ class mainPartOne {
             this.row = row;
             this.col = col;
         }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Beam beam = (Beam) obj;
+            return row == beam.row && col == beam.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row,col);
+        }
     }
 
 
 
     /// Parse through the input file to find 'S' or the starting position
-    public static int[] findS(String filePath) {
+    public static long solver() {
         try {
-            lines = Files.readAllLines(Paths.get(filePath));
-            grid = new char[lines.size()][];
-
+            var lines = Files.readAllLines(Paths.get("input.txt"));
+            char[][] grid = new char[lines.size()][];
+            
+            int startRow = -1, startCol = -1;
             for (int row = 0; row < lines.size(); row++) {
                 grid[row] = lines.get(row).toCharArray();
                 for (int col = 0; col < grid[row].length; col++) {
                     if (grid[row][col] == 'S') {
-                        int startRow = row;
-                        int startCol = col;
-                        return new int[]{startRow, startCol};
+                        startRow = row;
+                        startCol = col;
                     }
                 }
             }
+            
+            return simulateBeams(grid, startRow, startCol);
+            
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            System.err.println("Error reading file: " + e.getMessage());
+            return 0;
         }
-
-        return new int[]{-1, -1};
     }
+    
 
 
-    // Helper Function
+    // Helper Functions
     // From the index of 'S,' traverse the column until a split '^' is found
-    // If a split happens then add to the counter
-    private static int simulateBeams(int startRow, int startCol) {
-        
+    // If a split happens, add to the counter
+    private static long simulateBeams(char[][] grid, int startRow, int startCol) {
         Queue<Beam> activeBeams = new LinkedList<>();
-        Set<String> splitterActivated = new HashSet<>();
-        int splitCount = 0;
-        
-        return 0;
+        Set<String> splitterActivations = new HashSet<>();
+        long splitCount = 0;
+
+        activeBeams.add(new Beam(startRow, startCol));
+
+        while (!activeBeams.isEmpty()) {
+            Beam beam = activeBeams.poll();
+
+            while (beam.row < grid.length) {
+                // Beam leaves the grid
+                if (beam.col < 0 || beam.col >= grid[beam.row].length) break; 
+                if (grid[beam.row][beam.col] == '^') {
+                    String splitterKey = beam.row + "," + beam.col;
+                    
+                    if (!splitterActivations.contains(splitterKey)) {
+                        splitterActivations.add(splitterKey);
+                        splitCount++;
+                        
+                        if (beam.col - 1 >= 0) activeBeams.add(new Beam(beam.row + 1, beam.col - 1)); 
+                        if (beam.col + 1 < grid[beam.row].length) activeBeams.add(new Beam(beam.row + 1, beam.col + 1));
+                    }
+                    
+                    break;
+                }
+                beam.row++;
+            }
+        }
+        return splitCount;
     }
 
-
-
-    public static int solver() {
-
-        int[] start = findS("input.txt");
-
-        
-        return 0;
-    }
-
-
-
+    
     public static void main(String[] args) {
-        int[] test = findS("input.txt");
-        System.out.println(test[0] + ", " + test[1]);
+        System.out.println(solver());
     }
 }
