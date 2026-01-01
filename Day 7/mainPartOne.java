d5c31fa90db57fa21740be99432335abf9f091cf
RovasFlow <shan65123@gmail.com>
2026-01-01 17:36:25 -0500

    Added Beam class, starting simulationBeams function

--- Day 7/mainPartOne.java
+++ Day 7/mainPartOne.java
@@ -1,21 +1,43 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 class mainPartOne {
+
+    private static List<String> lines;
+    private static char[][] grid;
+
+
+
+    // Need to track the beams
+    static class Beam {
+        int row, col;
+
+        Beam(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+
 
     /// Parse through the input file to find 'S' or the starting position
     public static int[] findS(String filePath) {
         try {
-            var lines = Files.readAllLines(Paths.get(filePath));
-            char[][] grid = new char[lines.size()][];
+            lines = Files.readAllLines(Paths.get(filePath));
+            grid = new char[lines.size()][];
 
-            for (int i = 0; i < lines.size(); i++) {
-                grid[i] = lines.get(i).toCharArray();
-                for (int j = 0; j < grid[i].length; j++) {
-                    if (grid[i][j] == 'S') {
-                        int startRow = i;
-                        int startCol = j;
+            for (int row = 0; row < lines.size(); row++) {
+                grid[row] = lines.get(row).toCharArray();
+                for (int col = 0; col < grid[row].length; col++) {
+                    if (grid[row][col] == 'S') {
+                        int startRow = row;
+                        int startCol = col;
                         return new int[]{startRow, startCol};
                     }
                 }
@@ -28,17 +50,29 @@ class mainPartOne {
     }
 
 
+    // Helper Function
+    // From the index of 'S,' traverse the column until a split '^' is found
+    // If a split happens then add to the counter
+    private static int simulateBeams(int startRow, int startCol) {
+        
+        Queue<Beam> activeBeams = new LinkedList<>();
+        Set<String> splitterActivated = new HashSet<>();
+        int splitCount = 0;
+        
+        return 0;
+    }
+
+
 
     public static int solver() {
 
         int[] start = findS("input.txt");
 
-        // From the index of 'S,' traverse the column until a split '^' is found
-        
-        // If a split happens then add to the counter
         
         return 0;
     }
+
+
 
     public static void main(String[] args) {
         int[] test = findS("input.txt");
