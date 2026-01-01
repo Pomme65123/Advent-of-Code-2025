ec873448357d5d766252f80c34612bd73680b476
RovasFlow <shan65123@gmail.com>
2026-01-01 16:19:02 -0500

    Starting Day 7 Part 1

---
+++ Day 7/mainPartOne.java
@@ -0,0 +1,17 @@
+class mainPartOne {
+
+    public static int solver() {
+        
+        // Parse through the input file to find 'S'
+
+        // From the index of 'S,' traverse the column until a split '^' is found
+        
+        // If a split happens then add to the counter
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solver());
+    }
+}
