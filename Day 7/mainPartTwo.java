7d694a786a81ce6e4df4a411d62ad4c0cda216bd
RovasFlow <shan65123@gmail.com>
2026-01-02 15:07:37 -0500

    Solved Day 7 Part 2

--- Day 7/mainPartTwo.java
+++ Day 7/mainPartTwo.java
@@ -27,6 +27,7 @@ class mainPartTwo {
         public int hashCode() {
             return Objects.hash(row,col);
         }
+
     }
     
 
@@ -46,7 +47,8 @@ class mainPartTwo {
                     }
                 }
             }
-            return 0;
+            
+            return simulateBeams(grid, startRow, startCol);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
             return 0;
@@ -56,13 +58,64 @@ class mainPartTwo {
 
 
     private static long simulateBeams(char[][] grid, int startRow, int startCol)  {
+        // Instead of working with char[][] grid, we can use a hashmap
+        Map<Beam, Long> beamCounts = new HashMap<>();
+
         // Starting at 'S'
+        beamCounts.put(new Beam(startRow, startCol), 1L);
+
         // Beams will fall vertically until it reaches a '^'
-        // When it reaches a '^' it will split into two beams going left and right
-        // When the beam splits, a char representing the number will be placed in the cell
-        // If two splits overlap, the number will be the sum of the splits
+        for (int row = startRow; row < grid.length - 1; row++) {
+            Map<Beam, Long> nextRowCounts = new HashMap<>();
+
+            for (Map.Entry<Beam, Long> entry : beamCounts.entrySet()) {
+                Beam beam = entry.getKey();
+                Long count = entry.getValue();
+
+                if (beam.row != row) continue;
+                
+                char currentCell = grid[beam.row][beam.col];
+                
+                if (currentCell == '^') {
+                    // When it reaches a '^' it will split into two beams going left and right
+                    if (beam.col - 1 >= 0 && beam.col - 1 < grid[row + 1].length) {
+                        Beam leftBeam = new Beam(row + 1, beam.col - 1);
+                        nextRowCounts.merge(leftBeam, count, Long::sum);
+                    }
+
+                    if (beam.col + 1 >= 0 && beam.col + 1 < grid[row + 1].length) {
+                        Beam rightBeam = new Beam(row + 1, beam.col + 1);
+                        nextRowCounts.merge(rightBeam, count, Long::sum);
+                    }
+                } else {
+
+                    // If no '^', beam goes downwards
+                    if (beam.col >= 0 && beam.col < grid[row + 1].length) {
+                        Beam straightBeam = new Beam(row + 1, beam.col);
+                        nextRowCounts.merge(straightBeam, count, Long::sum);
+                    }
+                }
+            }
+
+            for (Map.Entry<Beam, Long> entry : nextRowCounts.entrySet()) {
+                beamCounts.put(entry.getKey(), entry.getValue());
+            }
+        }
+
         // Sum all the numbers in the final row
-        return 0;
+        long totalBeams = 0;
+        int finalRow = grid.length - 1;
+        for (Map.Entry<Beam, Long> entry : beamCounts.entrySet()) {
+            if (entry.getKey().row == finalRow) {
+                totalBeams += entry.getValue();
+            }
+        }
+
+        // for (Map.Entry<Beam, Long> entry : beamCounts.entrySet()) {
+        //     System.out.println("Beam at (" + entry.getKey().row + ", " + entry.getKey().col + "): " + entry.getValue());
+        // }
+        
+        return totalBeams;
     }
 
 
